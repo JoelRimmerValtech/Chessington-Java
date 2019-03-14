c2677dbb5dcb48cbdf57e14b8a919be03f73230c
Joel Rimmer <joel.rimmer@valtech.com>
2019-03-14 14:02:20 +0000

    diagonal takes

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -1,5 +1,6 @@
 package training.chessington.model.pieces;
 
+import org.apache.logging.log4j.core.Core;
 import org.apache.logging.log4j.core.pattern.AbstractStyleNameConverter;
 import training.chessington.model.Board;
 import training.chessington.model.Coordinates;
@@ -16,7 +17,9 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
+
         ArrayList<Move> pawnMoves = new ArrayList<>();
+
         if (colour.equals(PlayerColour.WHITE)) {
             if (from.getRow() > 0) {
                 Move move = new Move(from, from.plus(-1, 0));
@@ -49,6 +52,39 @@ public class Pawn extends AbstractPiece {
                 }
             }
         }
+        if (colour.equals(PlayerColour.WHITE)) {
+            if (from.getRow() != 0 && from.getCol() != 7) {
+                Move move = new Move(from, from.plus(-1, 1));
+                Coordinates enemyCoords = move.getTo();
+                if (board.get(enemyCoords) != null && board.get(enemyCoords).getColour().equals(PlayerColour.BLACK)) {
+                    pawnMoves.add(move);
+                }
+            }
+            if (from.getRow() != 0 && from.getCol() != 0) {
+                Move move = new Move(from, from.plus(-1, -1));
+                Coordinates enemyCoords = move.getTo();
+                if (board.get(enemyCoords) != null && board.get(enemyCoords).getColour().equals(PlayerColour.BLACK)) {
+                    pawnMoves.add(move);
+                }
+            }
+        }
+        if (colour.equals(PlayerColour.BLACK)) {
+            if (from.getRow() != 7 && from.getCol() != 7) {
+                Move move = new Move(from, from.plus(1, 1));
+                Coordinates enemyCoords = move.getTo();
+                if (board.get(enemyCoords) != null && board.get(enemyCoords).getColour().equals(PlayerColour.WHITE)) {
+                    pawnMoves.add(move);
+                }
+            }
+            if (from.getRow() != 7 && from.getCol() != 0) {
+                Move move = new Move(from, from.plus(1, -1));
+                Coordinates enemyCoords = move.getTo();
+                if (board.get(enemyCoords) != null && board.get(enemyCoords).getColour().equals(PlayerColour.WHITE)) {
+                    pawnMoves.add(move);
+                }
+            }
+        }
+
 
         return pawnMoves;
     }
